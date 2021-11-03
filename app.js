6edeb7602a6661c67293e3166619b3eb2746e347
Chi Duong <chiduong4107@gmail.com>
2021-11-03 00:16:40 -0400

    try different ways to implement search functionality

--- app.js
+++ app.js
@@ -1,31 +1,77 @@
 const albums = document.querySelector('.lists')
 const searchBar = document.querySelector('.searchbar')
 const count = document.querySelector('.count')
+const searchBtn = document.querySelector('button')
 
-searchBar.addEventListener('keyup', (e) => {
-  let searchValue = e.target.value
+/* keyup in js eventlisteners and hit enter */
+searchBar.addEventListener('keyup', () => {
+  if (event.key === 'Enter') {
+    let searchValue = searchBar.value
 
-  fetch(
-    `https://itunes.apple.com/search?term=${searchValue}&media=music&entity=album&attribute=artistTerm&limit=200`
-  )
-    .then((res) => res.json())
-    .then((data) => {
-      console.log('data: ', data.results)
+    fetch(
+      `https://itunes.apple.com/search?term=${searchValue}&media=music&entity=album&attribute=artistTerm&limit=200`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log('data: ', data.results)
 
-      displayLists(data.results, searchValue)
-    })
-    .catch(console.error)
+        displayLists(data.results, searchValue)
+      })
+      .catch(console.error)
+  }
 })
+
+/* onkeyup in HTML and hit enter */
+// const typed = (input) => {
+//   if (event.key === 'Enter') {
+//     let searchValue = input.value
+
+//     fetch(
+//       `https://itunes.apple.com/search?term=${searchValue}&media=music&entity=album&attribute=artistTerm&limit=200`
+//     )
+//       .then((res) => res.json())
+//       .then((data) => {
+//         displayLists(data.results, searchValue)
+//       })
+//       .catch(console.error)
+//   }
+// }
+
+/* click a button in js eventlisteners */
+// searchBtn.addEventListener('click', () => {
+//   let searchValue = searchBar.value
+
+//   console.log(searchValue)
+
+//   fetch(
+//     `https://itunes.apple.com/search?term=${searchValue}&media=music&entity=album&attribute=artistTerm&limit=200`
+//   )
+//     .then((res) => res.json())
+//     .then((data) => {
+//       displayLists(data.results, searchValue)
+//     })
+//     .catch(console.error)
+// })
+
+/* click a button in HTML */
+// const clicked = () => {
+//   let searchValue = searchBar.value
+
+//   fetch(
+//     `https://itunes.apple.com/search?term=${searchValue}&media=music&entity=album&attribute=artistTerm&limit=200`
+//   )
+//     .then((res) => res.json())
+//     .then((data) => {
+//       displayLists(data.results, searchValue)
+//     })
+//     .catch(console.error)
+// }
 
 const displayLists = (lists, searchVal) => {
   count.innerHTML = lists.length + ' results for ' + '"' + searchVal + '"'
 
-  albums.innerHTML = ''
-
   for (let i = 0; i < lists.length; i++) {
-    albums.appendChild(
-      displayList(lists[i].artworkUrl100, lists[i].collectionName)
-    )
+    albums.append(displayList(lists[i].artworkUrl100, lists[i].collectionName))
   }
 }
 
