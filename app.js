839d252bf139a2cb64a2b1cb6df90405b48c8e15
Chi Duong <chiduong4107@gmail.com>
2021-11-04 00:05:06 -0400

    add pagination

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
 const albums = document.querySelector('.lists')
 const searchBar = document.querySelector('.searchbar')
 const count = document.querySelector('.count')
-const searchBtn = document.querySelector('button')
+const searchBtn = document.querySelector('.searchbtn')
+const pagination = document.querySelector('.pagination')
+let currentPage = 1
+let items_per_page = 20
 
 /* keyup in js eventlisteners and hit enter */
-searchBar.addEventListener('keyup', () => {
-  if (event.key === 'Enter') {
-    let searchValue = searchBar.value
+searchBar.addEventListener('keyup', (event) => {
+  if (event.keyCode === 13) {
+    event.preventDefault()
+    // Trigger Button Click on Enter
+    searchBtn.click()
 
-    fetch(
-      `https://itunes.apple.com/search?term=${searchValue}&media=music&entity=album&attribute=artistTerm&limit=200`
-    )
-      .then((res) => res.json())
-      .then((data) => {
-        console.log('data: ', data.results)
+    // let searchValue = searchBar.value
 
-        displayLists(data.results, searchValue)
-      })
-      .catch(console.error)
+    // fetch(
+    //   `https://itunes.apple.com/search?term=${searchValue}&media=music&entity=album&attribute=artistTerm&limit=200`
+    // )
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //     console.log('data: ', data.results)
+
+    //     displayLists(data.results, searchValue)
+    //   })
+    //   .catch(console.error)
   }
 })
 
@@ -54,40 +61,90 @@ searchBar.addEventListener('keyup', () => {
 // })
 
 /* click a button in HTML */
-// const clicked = () => {
-//   let searchValue = searchBar.value
+const clicked = () => {
+  let searchValue = searchBar.value
 
-//   fetch(
-//     `https://itunes.apple.com/search?term=${searchValue}&media=music&entity=album&attribute=artistTerm&limit=200`
-//   )
-//     .then((res) => res.json())
-//     .then((data) => {
-//       displayLists(data.results, searchValue)
-//     })
-//     .catch(console.error)
-// }
+  fetch(
+    `https://itunes.apple.com/search?term=${searchValue}&media=music&entity=album&attribute=artistTerm&limit=200`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      displayLists(data, searchValue, currentPage, items_per_page)
+      displayPageBtns(data, items_per_page, searchValue)
+    })
+    .catch(console.error)
+}
 
-const displayLists = (lists, searchVal) => {
-  count.innerHTML = lists.length + ' results for ' + '"' + searchVal + '"'
+/* add a new album */
 
-  for (let i = 0; i < lists.length; i++) {
-    albums.append(displayList(lists[i].artworkUrl100, lists[i].collectionName))
+const displayLists = (lists, searchVal, currpage, itemsperpage) => {
+  count.innerHTML = lists.resultCount + ' results for ' + '"' + searchVal + '"'
+
+  albums.innerHTML = ''
+  currpage-- // 1 - 1 = 0
+
+  let start = currpage * itemsperpage // 0 * 20 = 0
+  let end = start + itemsperpage // 0 + 20 = 20
+
+  let ipp = lists.results.slice(start, end) // 0 to 19
+  // console.log(ipp)
+
+  for (let i = 0; i < ipp.length; i++) {
+    albums.append(displayList(ipp[i].artworkUrl100, ipp[i].collectionName))
   }
 }
 
 const displayList = (img, alb) => {
+  /* 
+  <li class='list__artist'>
+    <img class='list__artist-img' src='{img}' />
+    <p class='list__artist-album'>{alb}</p>
+  </li> 
+  */
+
   const artist = document.createElement('li')
-  artist.classList = 'list__artist'
+  artist.classList.add('list__artist')
 
   const image = document.createElement('img')
-  image.classList = 'list__artist-img'
+  image.classList.add('list__artist-img')
   image.setAttribute('src', img)
   artist.append(image)
 
   const album = document.createElement('p')
-  album.classList = 'list__artist-album'
+  album.classList.add('list__artist-album')
   album.textContent = alb
   artist.append(album)
 
   return artist
+}
+
+const displayPageBtns = (lists, itemsperpage, searchVal) => {
+  pagination.innerHTML = ''
+
+  let pageBtnsCount = Math.ceil(lists.results.length / itemsperpage) // 153 / 20 = 8
+
+  for (let i = 1; i <= pageBtnsCount; i++) {
+    pagination.append(pageBtn(lists, i, searchVal))
+  }
+}
+
+const pageBtn = (lists, pageNum, searchVal) => {
+  let pagebtn = document.createElement('button')
+  pagebtn.textContent = pageNum
+  pagebtn.classList.add('pagebtn')
+
+  if (currentPage == pageNum) pagebtn.classList.add('pagebtn-active') // make a current page button active
+
+  pagebtn.addEventListener('click', () => {
+    currentPage = pageNum
+
+    displayLists(lists, searchVal, currentPage, items_per_page) // when a button is clicked, display a clicked page
+
+    pagebtn.classList.add('pagebtn-active') // when a button is clicked, make it active
+
+    let currbtn = document.querySelector('.pagebtn-active') // make a previously active button not active
+    currbtn.classList.remove('pagebtn-active')
+  })
+
+  return pagebtn
 }
